b59b7dd94aa9cd61dcb4dc989665027b80f88086
Harshwardhan <harshawardhandudhal@gmail.com>
2026-06-15 20:44:33 +0530

    added the logic for delete and marks as completed tasks

--- 05.  Mini Challenges/Code02/script.js
+++ 05.  Mini Challenges/Code02/script.js
@@ -3,8 +3,9 @@
 const addTaskBtn = document.getElementById("addTaskBtn");
 const inputTask = document.getElementById("inputTask");
 const taskSection = document.querySelector(".taskSection");
+const deleteBtn = document.querySelector(".delete-btn");
 
-addTaskBtn.addEventListener("click", function () {
+function createTaskcontainer() {
   let div = document.createElement("div");
   div.classList.add("task-card");
 
@@ -19,11 +20,39 @@ addTaskBtn.addEventListener("click", function () {
 
   let deletebtn = document.createElement("button");
   deletebtn.classList.add("delete-btn");
-  deletebtn.innerText = "🗑";
+
+  let deleteicon = document.createElement("i");
+  deleteicon.classList.add("fa-regular", "fa-trash-can");
 
   div2.appendChild(checkbox);
   div2.appendChild(task);
+  deletebtn.appendChild(deleteicon);
   div.appendChild(div2);
   div.appendChild(deletebtn);
   taskSection.appendChild(div);
+}
+
+addTaskBtn.addEventListener("click", function () {
+  if (inputTask.value === "") {
+    return alert("Enter task");
+  } else {
+    createTaskcontainer();
+    inputTask.value = "";
+  }
+});
+
+taskSection.addEventListener("click", function (e) {
+  if (e.target.type === "checkbox") {
+    if (e.target.checked) {
+      e.target.closest(".task-card").classList.add("completed");
+    } else {
+      e.target.closest(".task-card").classList.remove("completed");
+    }
+  }
+});
+
+taskSection.addEventListener("click", function (e) {
+  if (e.target.closest(".delete-btn")) {
+    e.target.closest(".task-card").remove();
+  }
 });
