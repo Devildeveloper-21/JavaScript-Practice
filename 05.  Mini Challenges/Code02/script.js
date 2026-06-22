2ca105eabc0b91a78b90ba095fdd50ecc611ea93
Harshwardhan <harshawardhandudhal@gmail.com>
2026-06-22 21:26:39 +0530

    added logic to delete the task as  DOM element and object in array

--- 05.  Mini Challenges/Code02/script.js
+++ 05.  Mini Challenges/Code02/script.js
@@ -1,14 +1,16 @@
 //  Create a to-do list (add, delete, mark complete).
 
-let tasks =JSON.parse(localStorage.getItem("tasks")) || [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let id = 0;
 const addTaskBtn = document.getElementById("addTaskBtn");
 const inputTask = document.getElementById("inputTask");
 const taskSection = document.querySelector(".taskSection");
 const deleteBtn = document.querySelector(".delete-btn");
 
-function createTaskcontainer(text) {
+function createTaskcontainer(text, UID) {
   let div = document.createElement("div");
   div.classList.add("task-card");
+  div.id = UID;
 
   let div2 = document.createElement("div");
   div2.classList.add("left");
@@ -37,23 +39,40 @@ addTaskBtn.addEventListener("click", function () {
   if (inputTask.value === "") {
     return alert("Enter task");
   } else {
-    tasks.push({
+    const task = {
+      identity: id++,
       text: inputTask.value,
       mark: false,
-    });
+    };
+    tasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    createTaskcontainer(inputTask.value);
+    createTaskcontainer(task.text, task.identity);
     inputTask.value = "";
+    console.log(tasks);
   }
 });
 
 window.onload = loadTaskcards;
 function loadTaskcards() {
-  let savedTask = JSON.parse(localStorage.getItem("tasks"));
-  savedTask.forEach((element) => {
-    createTaskcontainer(element.text);
+  tasks.forEach((element) => {
+    createTaskcontainer(element.text, element.identity);
   });
 }
+
+taskSection.addEventListener("click", function (e) {
+  if (e.target.closest(".delete-btn")) {
+    let getidtoRemove = e.target.closest(".task-card").id;
+    e.target.closest(".task-card").remove();
+    for (const key in tasks) {
+      if (getidtoRemove == tasks[key].identity) {
+        console.log(tasks[key].identity);
+        tasks.splice(key, 1);
+      }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
+
 
 taskSection.addEventListener("click", function (e) {
   if (e.target.type === "checkbox") {
@@ -64,9 +83,4 @@ taskSection.addEventListener("click", function (e) {
     }
   }
 });
-
-taskSection.addEventListener("click", function (e) {
-  if (e.target.closest(".delete-btn")) {
-    e.target.closest(".task-card").remove();
-  }
-});
+// localStorage.clear();
